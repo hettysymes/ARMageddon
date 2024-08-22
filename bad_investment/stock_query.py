d57f2a931246686b39d8779a9ed7a1c437af4b4f
Hetty Symes <hetty.symes@gmail.com>
2024-08-22 14:40:18 +0100

    Retries more in case of error

--- bad_investment/stock_query.py
+++ bad_investment/stock_query.py
@@ -3,6 +3,7 @@ import requests
 from dotenv import load_dotenv
 import os
 from datetime import timedelta, datetime
+import time
 
 # Load the API Key
 load_dotenv()
@@ -11,17 +12,20 @@ API_KEY = os.getenv('TWELVE_DATA_API_KEY')
 # Get the price of a stock (symbol) at a given date (date)
 def get_price_at_date(symbol, date):
     DAYS_TO_CHECK = 10
+    TIMES_TO_CHECK_EACH_DAY = 2
     for _ in range(DAYS_TO_CHECK):
-        next_date = date + timedelta(days=1)
-        request_data_format = "%Y-%m-%d"
-        date_str = date.strftime(request_data_format)
-        next_date_str = next_date.strftime(request_data_format)
-        print(f"INFO:finding {symbol} stock between {date_str} and {next_date_str}")
-        request_str = f"https://api.twelvedata.com/time_series?apikey={API_KEY}&interval=1day&symbol={symbol}&start_date={date_str} 00:00:00&format=JSON&end_date={next_date_str} 00:00:00"
-        response = requests.get(request_str)
-        data = json.loads(response.text)
-        if data["status"] == "ok":
-            return {"price": float(data["values"][0]["close"]), "date": date, "success": True}
+        for _ in range(TIMES_TO_CHECK_EACH_DAY):
+            next_date = date + timedelta(days=1)
+            request_data_format = "%Y-%m-%d"
+            date_str = date.strftime(request_data_format)
+            next_date_str = next_date.strftime(request_data_format)
+            print(f"INFO:finding {symbol} stock between {date_str} and {next_date_str}")
+            request_str = f"https://api.twelvedata.com/time_series?apikey={API_KEY}&interval=1day&symbol={symbol}&start_date={date_str} 00:00:00&format=JSON&end_date={next_date_str} 00:00:00"
+            response = requests.get(request_str)
+            data = json.loads(response.text)
+            if data["status"] == "ok":
+                return {"price": float(data["values"][0]["close"]), "date": date, "success": True}
+            time.sleep(0.1)
         date -= timedelta(days=1)
     return {"price": "", "date": "", "success": False}
 
