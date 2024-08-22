6238f96e06a767bb525cc4eb9073f8054d75ec40
William Huynh <william.huynh@arm.com>
2024-08-22 14:07:16 +0100

    Add regex

--- directory_reconstruction/filetree.h
+++ directory_reconstruction/filetree.h
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-class FileTree {
+class FileTree
+{
     Directory *root;
     Directory *current;
     int total_files;
 
-    public:
+public:
     FileTree();
     void cd(string name);
     void add_file(string name);
     void add_directory(string name);
     int file_count();
-
 };
--- directory_reconstruction/main.cpp
+++ directory_reconstruction/main.cpp
@@ -2,45 +2,50 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <regex>
 
 #include "filetree.h"
 
-using namespace std;
+int main()
+{
+    static const std::regex CD_PATTERN(R"(\$ cd (.*))");
+    static const std::regex LS_PATTERN(R"(\$ ls)");
+    static const std::regex FILE_PATTERN(R"((\d+) (.*))");
+    static const std::regex DIR_PATTERN(R"(dir (.*))");
 
-int main() {
-    string path_to_file = "sample.txt";
+    std::string path_to_file = "sample.txt";
 
-    FileTree* ft = new FileTree();
-    delete ft;
+    FileTree *ft = new FileTree();
 
-    string line;
+    std::string line;
+    std::smatch match;
 
-    ifstream history_file;
+    std::ifstream history_file;
     history_file.open(path_to_file);
 
-    getline(history_file, line);
-
-    while (true) {
-        char cmd = line[2]; 
-        if (cmd == 'c') { // cd
-            string dir_name = line.substr(5);
+    while (getline(history_file, line))
+    {
+        char cmd = line[2];
+        if (std::regex_match(line, CD_PATTERN))
+        {
+            std::string dir_name = line.substr(5);
             ft->cd(dir_name);
             cout << "cd to " << dir_name << endl;
-            getline(history_file, line);
-        } else { // ls
+        }
+        else if (std::regex_match(line, LS_PATTERN))
+        {
             cout << "ls used" << endl;
-            getline(history_file, line);
-            while (line[0] != '$') {
-                string name = line.substr(line.find(" ")+1);
-                if (line[0] == 'd') { // directory
-                    cout << "add directory " << name << endl;
-                    ft->add_directory(name);
-                } else { // file
-                    cout << "add file " << name << "." << endl;
-                    ft->add_file(name);
-                }
-                getline(history_file, line);
-            }
+        }
+        else if (std::regex_match(line, match, DIR_PATTERN))
+        {
+            std::string name = match.str(1);
+            ft->add_directory(name);
+        }
+        else if (std::regex_match(line, match, FILE_PATTERN))
+        {
+            long filesize = std::stol(match.str(1));
+            std::string name = match.str(2);
+            ft->add_file(name);
         }
     }
     cout << ft->file_count() << "\n";
