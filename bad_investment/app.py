def75d9526413a8e8f04d6b32b815a83ce44f889
Hetty Symes <hetty.symes@gmail.com>
2024-08-22 15:43:54 +0100

    more

--- bad_investment/app.py
+++ bad_investment/app.py
@@ -11,15 +11,17 @@ def index():
         date_bought = datetime.strptime(request.form['date_bought'], "%Y-%m-%d")
         res = calculate_profit(stock_symbol, price_bought, date_bought)
         profit_msg = ""
+        add_msg = ""
         if res["success"]:
             profit = res["profit"]
             if (profit >= 0):
                 profit_msg = f"Your PROFIT is: {profit:.2f} USD"
             else:
                 profit_msg = f"Your LOSS is: {-profit:.2f} USD"
+            add_msg = f"Old price: {res['old_price']:.2f}\nNew price: {res['now_price']:.2f}\nNumber of stocks bought: {res['num_stock']:.2f}"
         else:
             profit_msg = "ERROR: could not find the price at this date in our system :("
-        return render_template('index.html', profit_msg=profit_msg)
-    return render_template('index.html', profit_msg="")
+        return render_template('index.html', profit_msg=profit_msg, add_msg=add_msg)
+    return render_template('index.html', profit_msg="", add_msg="")
 if __name__ == '__main__':
     app.run()
