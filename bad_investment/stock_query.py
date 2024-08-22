e62def8544123ed2c8ce204e5fee8d412af77fb6
Hetty Symes <hetty.symes@gmail.com>
2024-08-22 14:07:02 +0100

    Remove reliance on time value

--- bad_investment/stock_query.py
+++ bad_investment/stock_query.py
@@ -1,8 +1,4 @@
-
-# # for greater simplicity install our package
-# # https://github.com/twelvedata/twelvedata-python
 import json
-
 import requests
 from dotenv import load_dotenv
 import os
@@ -12,13 +8,14 @@ load_dotenv()
 API_KEY = os.getenv('TWELVE_DATA_API_KEY')
 
 def get_price_at_date(symbol, date):
-    for i in range(10):
+    DAYS_TO_TRY = 10
+    for i in range(DAYS_TO_TRY):
         next_date = date + timedelta(days=1)
-        request_data_format = "%Y-%m-%d %H:%M:%S"
+        request_data_format = "%Y-%m-%d"
         date_str = date.strftime(request_data_format)
         next_date_str = next_date.strftime(request_data_format)
         print(f"INFO:finding {symbol} stock between {date_str} and {next_date_str}")
-        request_str = f"https://api.twelvedata.com/time_series?apikey={API_KEY}&interval=1day&symbol={symbol}&start_date={date_str}&format=JSON&end_date={next_date_str}"
+        request_str = f"https://api.twelvedata.com/time_series?apikey={API_KEY}&interval=1day&symbol={symbol}&start_date={date_str} 00:00:00&format=JSON&end_date={next_date_str} 00:00:00"
         response = requests.get(request_str)
         data = json.loads(response.text)
         if data["status"] == "ok":
