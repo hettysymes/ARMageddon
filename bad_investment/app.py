0be84834851247caba5b4e3267e7fd37eb84bf0a
Hetty Symes <hetty.symes@gmail.com>
2024-08-22 13:52:28 +0100

    Website does not crash

--- bad_investment/app.py
+++ bad_investment/app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request
+from datetime import datetime
+from stock_query import calculate_profit
+
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Get the text from the textbox
-        amount_spent = request.form['tb_amount_spent']
-        return f"You entered: {amount_spent}"
+        stock_symbol = request.form['stock_symbol']
+        price_bought = float(request.form['price_bought'])
+        date_bought = datetime.strptime(request.form['date_bought'], "%Y-%m-%d")
+        res = calculate_profit(stock_symbol, price_bought, date_bought)
+        if res["success"]:
+            return str(res["profit"])
+        else:
+            return "ERROR: could not find the price at this date in our system :("
     return render_template('index.html')
 if __name__ == '__main__':
     app.run()
--- bad_investment/stock_query.py
+++ bad_investment/stock_query.py
@@ -9,24 +9,37 @@ import os
 from datetime import timedelta, datetime
 
 load_dotenv()
-# print(api_key)
+API_KEY = os.getenv('TWELVE_DATA_API_KEY')
 
-# response = requests.get()
-# dictt = json.loads(response.text)
-# with open("data.json", 'w') as f:
-#     json.dump(dictt, f, indent=2)
+def get_price_at_date(symbol, date):
+    for i in range(10):
+        next_date = date + timedelta(days=1)
+        request_data_format = "%Y-%m-%d %H:%M:%S"
+        date_str = date.strftime(request_data_format)
+        next_date_str = next_date.strftime(request_data_format)
+        print(f"INFO:finding ${symbol} stock between {date_str} and {next_date_str}")
+        request_str = f"https://api.twelvedata.com/time_series?apikey={API_KEY}&interval=1day&symbol={symbol}&start_date={date_str}&format=JSON&end_date={next_date_str}"
+        response = requests.get(request_str)
+        data = json.loads(response.text)
+        if data["status"] == "ok":
+            return {"price": float(data["values"][0]["close"]), "date": date, "success": True}
+        date -= timedelta(days=1)
+    return {"price": "", "date": "", "success": False}
 
-def get_price_at_date(symbol, date, api_key):
-    next_date = date + timedelta(days=1)
-    request_data_format = "%Y-%m-%d %H:%M:%S"
-    date_str = date.strftime(request_data_format)
-    next_date_str = next_date.strftime(request_data_format)
-    request_str = f"https://api.twelvedata.com/time_series?apikey={api_key}&interval=1day&symbol={symbol}&start_date={date_str}&format=JSON&end_date={next_date_str}"
-    response = requests.get(request_str)
-    data = json.loads(response.text)
-    return float(data["values"][0]["close"])
+def calculate_profit(symbol, price_bought, date_bought):
+    # Get price of stock at time of buy
+    old_price_res = get_price_at_date(symbol, date_bought)
+    if not old_price_res["success"]:
+        return {"profit": "", "success": False}
+    old_price = old_price_res["price"]
+    # Get number of stock bought
+    num_stock = price_bought/old_price
+    # Get price of stock now
+    now_price_res = get_price_at_date(symbol, datetime.today())
+    if not now_price_res["success"]:
+        return {"profit": "", "success": False}
+    now_price = now_price_res["price"]
+    # Return profit
+    return {"profit": num_stock*(now_price - old_price), "success": True}
 
-inp_date = "2024-08-20"
-date = datetime.strptime(inp_date, "%Y-%m-%d")
-print(get_price_at_date("ARM", date, os.getenv('TWELVE_DATA_API_KEY')))
     
