3477815e7426714d48216019b31c8504dc69f3c6
Wallace Chak Sum Leung <walleu01@cwd0qqhffy.cambridge.arm.com>
2024-08-22 15:31:16 +0100

    final

--- bad_investment/stock_query.py
+++ bad_investment/stock_query.py
@@ -1,31 +1,30 @@
-
-# # for greater simplicity install our package
-# # https://github.com/twelvedata/twelvedata-python
 import json
-
 import requests
 from dotenv import load_dotenv
 import os
 from datetime import timedelta, datetime
 
+# Load the API Key
 load_dotenv()
 API_KEY = os.getenv('TWELVE_DATA_API_KEY')
 
+# Get the price of a stock (symbol) at a given date (date)
 def get_price_at_date(symbol, date):
-    for i in range(10):
-        next_date = date + timedelta(days=1)
-        request_data_format = "%Y-%m-%d %H:%M:%S"
-        date_str = date.strftime(request_data_format)
-        next_date_str = next_date.strftime(request_data_format)
-        print(f"INFO:finding {symbol} stock between {date_str} and {next_date_str}")
-        request_str = f"https://api.twelvedata.com/time_series?apikey={API_KEY}&interval=1day&symbol={symbol}&start_date={date_str}&format=JSON&end_date={next_date_str}"
-        response = requests.get(request_str)
-        data = json.loads(response.text)
-        if data["status"] == "ok":
-            return {"price": float(data["values"][0]["close"]), "date": date, "success": True}
-        date -= timedelta(days=1)
+    request_data_format = "%Y-%m-%d"
+    to_date = date + timedelta(days=1)
+    from_date = date - timedelta(days=10)
+    to_date_str = to_date.strftime(request_data_format)
+    from_date_str = from_date.strftime(request_data_format)
+    request_str = f"https://api.twelvedata.com/time_series?apikey={API_KEY}&interval=1day&symbol={symbol}&start_date={from_date_str} 00:00:00&format=JSON&end_date={to_date_str} 00:00:00"
+    print(f"INFO:finding {symbol} stock between {from_date_str} and {to_date_str}")
+    response = requests.get(request_str)
+    data = json.loads(response.text)
+    if data["status"] == "ok":
+        # Get result found from most recent date
+        return {"price": float(data["values"][0]["close"]), "date": date, "success": True}
     return {"price": "", "date": "", "success": False}
 
+# Calculate the profit
 def calculate_profit(symbol, price_bought, date_bought):
     # Get price of stock at time of buy
     old_price_res = get_price_at_date(symbol, date_bought)
@@ -41,5 +40,3 @@ def calculate_profit(symbol, price_bought, date_bought):
     now_price = now_price_res["price"]
     # Return profit
     return {"profit": num_stock*(now_price - old_price), "success": True}
-
-    
